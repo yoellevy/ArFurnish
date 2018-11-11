4c4d12fe1b0bdd150bddc3be56c4d53bd75c29e2
yoellevy <yoel.levy@mail.huji.ac.il>
2018-11-11 21:06:36 +0200

    removing floating button

--- app/src/main/java/com/arfurnish/uxteam/arfurnish/MainActivity.java
+++ app/src/main/java/com/arfurnish/uxteam/arfurnish/MainActivity.java
@@ -55,18 +55,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-        // TODO: remove
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         fragment = (ArFragment)
                 getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
